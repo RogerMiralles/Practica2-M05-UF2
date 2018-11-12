a24201e0e38a479e830b9342f6f407513f933b5e
RogerMiralles <38457253+RogerMiralles@users.noreply.github.com>
2018-11-12 19:37:29 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/miral/practica2/MainActivity.java
+++ app/src/main/java/com/example/miral/practica2/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        phosky tonto;
-        1;
+        //hola oscar, esto es una pruba para hacer un pull
     }
 }
